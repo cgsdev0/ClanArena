299396ec8a5b43caa606f9de9b660c79d3e1ec32
Shane Schulte <schulte.shane@gmail.com>
2018-09-23 02:42:32 -0400

    Initial commit

---
+++ src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java
@@ -0,0 +1,16 @@
+package com.shaneschulte.mc.clanarena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClanArena extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}

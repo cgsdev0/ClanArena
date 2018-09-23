c185d6171d153b4b82032a4414ef87a9eee83079
Hikeru <russellsorin2@gmail.com>
2018-09-23 15:08:31 -0500

    Added Command Manager and Message Manager utils. Registered basic command

--- src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java
+++ src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java
@@ -4,9 +4,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClanArena extends JavaPlugin {
 
+    public static MessageManager messageManager = new MessageManager();
+
     @Override
     public void onEnable() {
-
+        this.getCommand("ClanArena").setExecutor(new CommandManager());
+        messageManager.logMessage("~Commands registered!~");
     }
 
     @Override

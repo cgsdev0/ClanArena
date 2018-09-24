67a23188189147e81ea39863932d072c84358a89
Hikeru <russellsorin2@gmail.com>
2018-09-23 19:40:04 -0500

    Added basic autocomplete for registered commands!

--- src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java
+++ src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java
@@ -1,13 +1,16 @@
 package com.shaneschulte.mc.clanarena;
 
+import com.shaneschulte.mc.clanarena.events.OnJoin;
 import com.shaneschulte.mc.clanarena.utils.ConstructTabCompleter;
 import com.shaneschulte.mc.clanarena.utils.MsgUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClanArena extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getPluginManager().registerEvents(new OnJoin(), this);
         this.getCommand("ClanArena").setExecutor(new CommandHandler());
         this.getCommand("ClanArena").setTabCompleter(new ConstructTabCompleter());
         MsgUtils.log("~Commands registered!~");
---
+++ src/main/java/com/shaneschulte/mc/clanarena/events/OnJoin.java
@@ -0,0 +1,16 @@
+package com.shaneschulte.mc.clanarena.events;
+
+import com.shaneschulte.mc.clanarena.utils.MsgUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class OnJoin implements Listener {
+    @EventHandler
+    public void onLogin(PlayerJoinEvent event) {
+        MsgUtils.sendMessage(event.getPlayer(), "Welcome! Message board:");
+        MsgUtils.raw(event.getPlayer(), "-= " + MsgUtils.Colors.HIGHLIGHT + "clan1" + MsgUtils.Colors.INFO + " claimed victory over " + MsgUtils.Colors.HIGHLIGHT + "clan2" + MsgUtils.Colors.INFO + " =-");
+        MsgUtils.raw(event.getPlayer(), "-= there are no other messages    =-");
+    }
+}

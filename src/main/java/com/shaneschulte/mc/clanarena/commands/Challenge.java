5251f05d819ca30b3333671e1b20b086c82c3477
Issac <iirlas@yahoo.com>
2018-09-23 21:28:51 -0500

    Revised src\main\java\com\shaneschulte\mc\clanarena\commands\Challenge.java

--- src/main/java/com/shaneschulte/mc/clanarena/commands/Challenge.java
+++ src/main/java/com/shaneschulte/mc/clanarena/commands/Challenge.java
@@ -6,11 +6,8 @@ import com.shaneschulte.mc.clanarena.events.OnChallengeStart;
 import com.shaneschulte.mc.clanarena.utils.CmdProperties;
 import com.shaneschulte.mc.clanarena.utils.MsgUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
-import java.util.Random;
 
 public class Challenge implements CmdProperties {
 
@@ -19,16 +16,9 @@ public class Challenge implements CmdProperties {
 
     @Override
     public void perform(Player player, String allArgs, String[] args) {
-        if (args.length != 2)
-        {
-            MsgUtils.sendMessage(player, "invalid number of arguments");
-            return;
-        }
 
-        Group challengers = GroupManager.get().getByTag(player.getName());
-        Group opponents = GroupManager.get().getByTag(args[0]);
-        int groupSize = Integer.min(Integer.min(Integer.parseInt(args[1]), challengers.members.size()), opponents.members.size());
-
+        Group challengers = GroupManager.get().getByPlayer(player);
+        Group opponents = GroupManager.get().getByTag(args[1]);
         if (challengers == null || opponents == null)
         {
             MsgUtils.sendMessage(player, "Group not found");
@@ -38,8 +28,10 @@ public class Challenge implements CmdProperties {
         if (challengers.name == opponents.name)
         {
             MsgUtils.sendMessage(player, "You can't challenge yourself");
+            return;
         }
 
+        int groupSize = Integer.min(Integer.min(Integer.parseInt(args[2]), challengers.members.size()), opponents.members.size());
         if (groupSize <= 0) {
             MsgUtils.sendMessage(player, "Too few players");
             return;
@@ -55,17 +47,17 @@ public class Challenge implements CmdProperties {
 
     @Override
     public String getCommand() {
-        return "Challenge";
+        return "challenge";
     }
 
     @Override
     public int getLength() {
-        return 1;
+        return 2;
     }
 
     @Override
     public String getUsage() {
-        return "/ca Challenge <player>";
+        return "/ca Challenge <group> <group size>";
     }
 
     @Override

77afb939dfa1b6fc2e53688052212c643ab85902
Issac <iirlas@yahoo.com>
2018-09-23 20:42:24 -0500

    SHA-1: 345db320383a08c01b84266a468ffdd28ce515ba
    * Merge remote-tracking branch 'remotes/origin/CommandManager' into iirlas
    
    # Conflicts:
    #	src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java

--- src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java
+++ src/main/java/com/shaneschulte/mc/clanarena/ClanArena.java
@@ -1,25 +1,32 @@
 package com.shaneschulte.mc.clanarena;
 
-import com.shaneschulte.mc.clanarena.events.OnJoin;
-import com.shaneschulte.mc.clanarena.listeners.ClanArenaListener;
+import com.shaneschulte.mc.clanarena.inventory.KitManager;
 import com.shaneschulte.mc.clanarena.utils.ConstructTabCompleter;
 import com.shaneschulte.mc.clanarena.utils.MsgUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ClanArena extends JavaPlugin {
 
+    private static ClanArena instance;
+
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new OnJoin(), this);
+        instance = this;
+        MsgUtils.log("~Commands registered!~");
+        KitManager.loadLoadouts();
+
         this.getCommand("ClanArena").setExecutor(new CommandHandler());
         this.getCommand("ClanArena").setTabCompleter(new ConstructTabCompleter());
         MsgUtils.log("~Commands registered!~");
-
-        getServer().getPluginManager().registerEvents(new ClanArenaListener(this), this);
     }
 
     @Override
     public void onDisable() {
-
+        instance = null;
     }
+
+    public static ClanArena getPlugin() {
+        return instance;
+    }
+
 }

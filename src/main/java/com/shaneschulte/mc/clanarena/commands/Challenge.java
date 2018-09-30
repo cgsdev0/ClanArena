cdb2e33dc35a42f516ba4e94b8703445cad5ebe5
Issac <iirlas@yahoo.com>
2018-09-29 23:25:44 -0500

    Revised src\main\java\com\shaneschulte\mc\clanarena\commands\Challenge.java

--- src/main/java/com/shaneschulte/mc/clanarena/commands/Challenge.java
+++ src/main/java/com/shaneschulte/mc/clanarena/commands/Challenge.java
@@ -1,12 +1,18 @@
 package com.shaneschulte.mc.clanarena.commands;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.shaneschulte.mc.clanarena.ClanArena;
 import com.shaneschulte.mc.clanarena.Group;
 import com.shaneschulte.mc.clanarena.adapters.GroupManager;
-import com.shaneschulte.mc.clanarena.events.OnChallengeStart;
+import com.shaneschulte.mc.clanarena.protocols.RespawnHandler;
 import com.shaneschulte.mc.clanarena.utils.AutoCompletable;
 import com.shaneschulte.mc.clanarena.utils.CmdProperties;
 import com.shaneschulte.mc.clanarena.utils.MsgUtils;
-import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -23,9 +29,13 @@ public class Challenge implements CmdProperties, AutoCompletable {
 
         Group challengers = GroupManager.get().getByPlayer(player);
         Group opponents = GroupManager.get().getByTag(args[1]);
-        if (challengers == null || opponents == null)
-        {
-            MsgUtils.sendMessage(player, "Group not found");
+
+        if (challengers == null) {
+            MsgUtils.sendMessage(player, "You are not in a group");
+            return;
+        }
+        if (opponents == null) {
+            MsgUtils.sendMessage(player, "Group [" + args[1] + "] not found");
             return;
         }
 
@@ -39,14 +49,19 @@ public class Challenge implements CmdProperties, AutoCompletable {
         if (groupSize <= 0) {
             MsgUtils.sendMessage(player, "Too few players");
             return;
-        }
-        else {
-            challengers.members.removeIf(member -> challengers.members.size() > groupSize);
-            opponents.members.removeIf(member -> opponents.members.size() > groupSize);
+        } else {
+            challengers.members.removeIf(member -> challengers.members.size() > groupSize || !member.isOnline());
+            opponents.members.removeIf(member -> opponents.members.size() > groupSize || !member.isOnline());
         }
 
-        OnChallengeStart event = new OnChallengeStart(challengers, opponents);
-        Bukkit.getPluginManager().callEvent(event);
+        challengers.members.forEach(member -> member.getPlayer().setGameMode(GameMode.ADVENTURE));
+        opponents.members.forEach(member -> {
+            MsgUtils.sendMessage((CommandSender) member, "You are being challenged by " + challengers.name);
+            MsgUtils.sendMessage((CommandSender) member, "Type [...] to opt-out");
+            member.getPlayer().setGameMode(GameMode.ADVENTURE);
+        });
+
+        ProtocolLibrary.getProtocolManager().addPacketListener(new RespawnHandler(ClanArena.getPlugin(), ListenerPriority.NORMAL, challengers, opponents, PacketType.Play.Server.UPDATE_HEALTH));
     }
 
     @Override
